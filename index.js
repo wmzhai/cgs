8038330db90759e79d6478b899594695bdd4662a
Weiming Zhai <wmzhai@gmail.com>
2017-02-16 15:45:11 +0800

    refactoring

--- index.js
+++ index.js
@@ -19,31 +19,21 @@ if (currentNodeVersion.split('.')[0] < 4) {
     process.exit(1);
 }
 
-var commander = require('commander');
-var projectName;
+const argv = minimist(process.argv.slice(2));
+const commands = argv._;
 
-var program = commander
-    .version(require('./package.json').version)
-    .arguments('<project-directory>')
-    .usage(chalk.green('<project-directory>') + ' [options]')
-    .action(function (name) {
-        projectName = name;
-    })
-    .option('-v, --verbose', 'print additional logs')
-    .allowUnknownOption()
-    .parse(process.argv);
+if (commands[0] === 'init') {
+    var projectName = commands[1];
+    if (!projectName) {
+        usage();
+    }
 
-if (typeof projectName === 'undefined') {
-    console.error('Please specify the project directory:');
-    console.log('  ' + chalk.cyan(program.name()) + chalk.green(' <project-directory>'));
-    console.log();
-    console.log('For example:');
-    console.log('  ' + chalk.cyan(program.name()) + chalk.green(' my-graphql-server'));
-    console.log();
-    console.log('Run ' + chalk.cyan(program.name() + ' --help') + ' to see all options.');
-    process.exit(1);
+    cgs.init(projectName);
+} else if (commands[0] === 'add') {
+
+} else {
+    usage();
 }
-
 
 function usage() {
     console.log('Usage: cgs <command> <arg>, where command is:');
@@ -51,23 +41,3 @@ function usage() {
     console.log(' - cgs add path/to/type.graphql');
     process.exit(1);
 }
-
-
-const argv = minimist(process.argv.slice(2));
-
-const commands = argv._;
-console.log('commands: ' + commands);
-
-if (commands[0] === 'init') {
-    const projectName = commands[1];
-    if (!projectName) {
-        usage();
-    }
-
-    cgs.createGraphQLServer(projectName);
-} else if (commands[0] === 'add') {
-
-} else {
-    usage();
-}
-
